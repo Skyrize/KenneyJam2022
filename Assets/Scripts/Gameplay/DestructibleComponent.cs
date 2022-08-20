0d7bdb43eee896d35c9b069a4b407c5aee600597
Jules <jules.girard5@gmail.com>
2022-08-21 01:49:50 +0200

    Show object bottom after destruction

--- Assets/Scripts/Gameplay/DestructibleComponent.cs
+++ Assets/Scripts/Gameplay/DestructibleComponent.cs
@@ -15,6 +15,7 @@ public class DestructibleComponent : MonoBehaviour
     private float m_shakeAmplitude = 0.0f;
     private float m_shakeAmplitudeMax = 0.5f;
     private List<GameObject> m_spawnedSurvivors;
+    private bool m_destroyed = false;
 
     private void Awake()
     {
@@ -36,13 +37,13 @@ public class DestructibleComponent : MonoBehaviour
             return;
         }
 
-        Vector3 position = transform.position;
-        position.x = m_initialPosition.x + Mathf.Sin(m_shakeTimer.ElapsedTime * 20) * m_shakeAmplitude;
-        position.z = m_initialPosition.z + Mathf.Sin(m_shakeTimer.ElapsedTime + 100 * 16) * m_shakeAmplitude;
+        Vector3 shakedPosition = transform.position;
+        shakedPosition.x = m_initialPosition.x + Mathf.Sin(m_shakeTimer.ElapsedTime * 20) * m_shakeAmplitude;
+        shakedPosition.z = m_initialPosition.z + Mathf.Sin(m_shakeTimer.ElapsedTime + 100 * 16) * m_shakeAmplitude;
 
         if (m_healthComponent.isDead)
         {
-            position.y -= 4.0f * Time.deltaTime;
+            shakedPosition.y -= 4.0f * Time.deltaTime;
 
             if (transform.position.y + m_height < 0.0f)
             {
@@ -51,15 +52,31 @@ public class DestructibleComponent : MonoBehaviour
                     survivor.GetComponent<Rigidbody>().detectCollisions = true;
                     survivor.GetComponent<SurvivorController>().enabled = true;
                 }
-                Destroy(gameObject);
+
+                gameObject.GetComponent<Rigidbody>().detectCollisions = false;
+
+                Vector3 position = transform.position;
+                position.y = 0.4f;
+                transform.position = position;
+
+                Vector3 scale = transform.localScale;
+                scale.y = -1;
+                transform.localScale = scale;
+
+                enabled = false;
+                m_destroyed = true;
+                return;
             }
         }
-
-        transform.position = position;
+        
+        transform.position = shakedPosition;
     }
 
     private void OnDamage(float _damagePoints)
     {
+        if (m_destroyed)
+            return;
+
         enabled = true;
         if (!m_shakeTimer.IsStarted)
             m_shakeTimer.Restart();
@@ -69,20 +86,26 @@ public class DestructibleComponent : MonoBehaviour
 
     private void OnDeath()
     {
+        if (m_destroyed)
+            return;
+
         enabled = true;
         m_shakeAmplitude = m_shakeAmplitudeMax;
-        m_spawnedSurvivors = new List<GameObject>((int)m_nbSurvivorsInside);
 
+        Vector3 bodyCenter = GetComponent<Rigidbody>().centerOfMass;
+        bodyCenter.y = 0.0f;
+        m_spawnedSurvivors = new List<GameObject>((int)m_nbSurvivorsInside);
         for (uint i = 0; i < m_nbSurvivorsInside; ++i)
         {
             Vector2 spawnPosition2D = Random.insideUnitCircle * Random.Range(0.5f, m_survivorSpawnRadius);
-            Vector3 spawnPosition = transform.position + new Vector3(spawnPosition2D.x, 0.0f, spawnPosition2D.y);
+            Vector3 spawnPosition = transform.position + bodyCenter + new Vector3(spawnPosition2D.x, 0.0f, spawnPosition2D.y);
             Vector2 spawnDirection2D = Random.insideUnitCircle.normalized;
             Quaternion spawnQuat = Quaternion.LookRotation(new Vector3(spawnDirection2D.x, 0.0f, spawnDirection2D.y));
 
             GameObject survivor = GameManager.Instance.SpawnManager.SpawnSurvivor(spawnPosition, spawnQuat);
             survivor.GetComponent<Rigidbody>().detectCollisions = false;
             survivor.GetComponent<SurvivorController>().enabled = false;
+            survivor.transform.position = spawnPosition;
             m_spawnedSurvivors.Add(survivor);
         }
     }
